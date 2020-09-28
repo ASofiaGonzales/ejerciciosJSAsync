4f84e8be3343de27ddba29bb26249217fea0e35d
sofia SVF <sofia.gonzales@live.com.ar>
2020-09-27 23:13:17 -0300

    ejercicio 1, ejercicio 2 en proceso

--- ejercicio_1/app.js
+++ ejercicio_1/app.js
@@ -3,9 +3,11 @@ const obtenerChiste = require("./library");
 
 // Codigo funcion callback
 
-
+function callback (chiste){
+    console.log(chiste)
+}
 
 // Fin codigo
 
 // Usar la funcion obtenerChiste(funcionCallback) en donde funcionCallback es una función que recibe el objeto chiste extraido
-obtenerChiste();
+obtenerChiste(callback);
